b4dbcad0a41dde9bf6126c5c48fc67dec6182278
Matthew Morley <matthew.morley.ca@gmail.com>
2019-10-26 14:03:24 -0700

    Update TurnToAngleCommand.kt

--- src/main/kotlin/frc/robot/subsystems/drive/TurnToAngleCommand.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/TurnToAngleCommand.kt
@@ -1,5 +1,6 @@
 package frc.robot.subsystems.drive
 
+import edu.wpi.first.wpilibj.controller.PIDController
 import org.ghrobotics.lib.commands.FalconCommand
 import org.ghrobotics.lib.mathematics.twodim.geometry.Rotation2d
 import org.ghrobotics.lib.mathematics.units.derived.degree
@@ -8,6 +9,7 @@ import org.ghrobotics.lib.mathematics.units.derived.toRotation2d
 class TurnToAngleCommand : FalconCommand(DriveSubsystem) {
 
     lateinit var wantedAngle: Rotation2d
+    val controller = PIDController(kP, 0.0, kD)
 
     override fun initialize() {
         wantedAngle = DriveSubsystem.gyro() + 90.degree.toRotation2d()
@@ -19,13 +21,14 @@ class TurnToAngleCommand : FalconCommand(DriveSubsystem) {
         val error = /* measured minus setpoint */ (DriveSubsystem.gyro() - wantedAngle).radian
 
         // output is error times kp
-        val leftOutput = error * kP
+        val leftOutput = controller.calculate(error, 0.0)
 
         DriveSubsystem.tankDrive(leftOutput, -leftOutput)
     }
 
     companion object {
         const val kP = 0.1
+        const val kD = 1.0
     }
 
 }
