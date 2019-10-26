a8f47bce409d6a6e7a3638a6c54662d3726a20ba
Matthew Morley <matthew.morley.ca@gmail.com>
2019-10-26 13:53:09 -0700

    Update DriveSubsystem.kt

--- src/main/kotlin/frc/robot/subsystems/drive/DriveSubsystem.kt
+++ src/main/kotlin/frc/robot/subsystems/drive/DriveSubsystem.kt
@@ -1,11 +1,18 @@
 package frc.robot.subsystems.drive
 
+import asSource
+import com.kauailabs.navx.frc.AHRS
+import edu.wpi.first.wpilibj.Compressor
+import edu.wpi.first.wpilibj.SPI
 import org.ghrobotics.lib.commands.FalconSubsystem
 import org.ghrobotics.lib.mathematics.units.nativeunit.DefaultNativeUnitModel
 import org.ghrobotics.lib.mathematics.units.nativeunit.NativeUnit
 import org.ghrobotics.lib.motors.ctre.FalconSRX
 
 object DriveSubsystem : FalconSubsystem() {
+
+    val gyro = AHRS(SPI.Port.kMXP).asSource()
+    val compressor = Compressor(9)
 
     val leftMotor: FalconSRX<NativeUnit> = FalconSRX(id = 1, model = DefaultNativeUnitModel).apply { /* this: FalconSRX<NativeUnit> */
         talonSRX.configFactoryDefault()
